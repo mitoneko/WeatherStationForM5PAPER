b926acddec47e15332b3c563b12e67500e6c64e9
mitoneko <mitoneko@laki.jp>
2021-03-22 22:05:50 +0900

    天気予報表示完了。写真表示追加。
    天気予報の取得と表示実装。
    メイン関数修正。
    一通り、初期目標を完了。

--- src/drawtenki.hpp
+++ src/drawtenki.hpp
@@ -8,13 +8,19 @@
 class DrawTenki {
     private:
         Tenki *tenki;
+        LGFX_Sprite screen;
         int width;
         int height;
+        float columnX[4];
+        float rowCenterY[3];
 
     public:
         DrawTenki(Tenki *tenki, int width, int height);
         void draw(LovyanGFX *lcd, int x, int y) ;
 
     private:
+        void drawFrameBorder() ;
+        void calcColumnCoordinate();
+        void drawTenkiInfo(int lineNo) ;
 
 };
